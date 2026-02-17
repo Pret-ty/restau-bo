e625aa29a14100494c641e3c98ac0be62c199254
Pret-ty <tchepretty@gmail.com>
2026-02-17 14:48:07 +0100

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => explode(',', env('ALLOWED_ORIGINS', 'http://localhost:3000,http://localhost:3001,http://localhost:5173,http://127.0.0.1:3000')),
+    'allowed_origins' => explode(',', env('ALLOWED_ORIGINS', 'http://localhost:3000,http://localhost:3001,http://localhost:5173,http://127.0.0.1:3000,https://restau-fe-wdxn.vercel.app')),
 
     'allowed_origins_patterns' => explode(',', env('ALLOWED_ORIGINS_PATTERNS', '')),
 
